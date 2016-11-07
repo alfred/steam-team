ecf8635bd6a1a5afbcfa54aa06239d46de489ed6
Alfred Ababio <aababio75@gmail.com>
2016-11-07 11:56:57 -0500

    These are tests for the mysql connector module

--- test/lib/db/mysql.js
+++ test/lib/db/mysql.js
@@ -11,5 +11,75 @@ describe( 'lib/mysql', () => {
     assert.isFunction( db );
   });
 
+  it ( 'should resolve', done => {
+    let db = require('rewire')( modulePath );
+
+    db.__set__( 'mysql', {
+      createConnection() {
+        return {
+          connect( cb ) {
+            cb();
+          }
+        }
+      }
+    });
+
+    db()
+    .then( conn => done() )
+    .catch( err => {
+      assert.ok( false );
+      done( err );
+    });
+  });
+
+  it ( 'should resolve fast with promise in scope', done => {
+    let db = require('rewire')( modulePath );
+
+    db.__set__( 'mysql', {
+      createConnection() {
+        return {
+          connect( cb ) {
+            cb();
+          }
+        }
+      }
+    });
+
+    db()
+    .then( conn1 => {
+      db().then( conn2 => {
+        assert.equal( conn1, conn2 );
+        done();
+      });
+    })
+    .catch( err => {
+      assert.ok( false );
+      done();
+    });
+  });
+
+  it ( 'should reject with an error', done => {
+    let db = require('rewire')( modulePath );
+
+    db.__set__( 'mysql', {
+      createConnection() {
+        return {
+          connect( cb ) {
+            cb( new Error() );
+          }
+        }
+      }
+    });
+
+    db()
+    .then( conn => {
+      assert.ok( false, 'Was supposed to error' );
+      done();
+    })
+    .catch( err => {
+      assert.ok( true );
+      done();
+    });
+  });
 
 });
